333654ed27c8dee4f4758bf8ee82c4f0d6f5ccd3
Tharnickanth <tharnimo.com>
2024-04-21 12:13:44 +0530

    get the name from player and display welcome message with name

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        String welcome_message = "Enter your Name:";
+        String player_name;
+        System.out.print(welcome_message);
+        Scanner scanner = new Scanner(System.in);
+        player_name = scanner.nextLine();
+        System.out.println("Hi...! "+player_name);
 
     }
 }
