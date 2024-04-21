f910989713d40c970f9293ef3b3c946133a1adb2
Tharnickanth <tharnimo.com>
2024-04-21 16:03:11 +0530

    game has created

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,10 +8,34 @@ public class Main {
 
         String welcome_message = "Enter your Name:";
         String player_name;
+        int random_number;
+        int guess_number;
         System.out.print(welcome_message);
         Scanner scanner = new Scanner(System.in);
         player_name = scanner.nextLine();
         System.out.println("Hi...! "+player_name);
+
+        Random random = new Random();
+        random_number = random.nextInt(20);
+        System.out.println("The system has generated a number within 20. Guess that number....");
+        int index=1;
+        do{
+            System.out.print("Enter the number (guess "+index+"): ");
+            Scanner scanner1 = new Scanner(System.in);
+            guess_number=Integer.parseInt(scanner1.nextLine());
+
+            if (guess_number==random_number){
+                System.out.println("Congratulations! You guessed the correct number.");
+                break;
+            }else{
+                index++;
+            }
+        }while(index<=5);
+
+        if(index>5){
+
+            System.out.println("Sorry, you have exceeded the maximum number of attempts.\nSystem generated number is "+random_number);
+        }
 
     }
 }
